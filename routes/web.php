6bdb043a9cbbff11df7fc84837609c0eb8c43cca
MSH38 <m.samy1011997@gmail.com>
2023-03-20 05:40:58 +0200

    create invoices controller , model ,view

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('invoices', 'InvoicesController');
 Route::get('/{page}', 'AdminController@index');
