cb96d311abe83b931bd5673f648aefe165ffbb7d
MSH38 <m.samy1011997@gmail.com>
2023-04-11 12:31:10 +0200

    Customize home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\invoices;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +23,128 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $chartjs2 = app()->chartjs
+        ->name('lineChartTest')
+        ->type('line')
+        ->size(['width' => 400, 'height' => 200])
+        ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
+        ->datasets([
+            [
+                "label" => "My First dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [65, 59, 80, 81, 56, 55, 40],
+            ],
+            [
+                "label" => "My Second dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [12, 33, 44, 44, 55, 23, 40],
+            ]
+        ])
+        ->options([]);
+
+    //     $chartjs = app()->chartjs
+    //      ->name('barChartTest')
+    //      ->type('bar')
+    //      ->size(['width' => 350, 'height' => 200])
+    //      ->labels(['الفواتير المدفوعة', 'الفواتير الغير مدفوعة'])
+    //      ->datasets([
+    //          [
+    //              "label" => "الفواتير المدفوعة",
+    //              'backgroundColor' => ['rgba(41, 150, 230, 0.3)', "rgba(38, 185, 140, 0.5)"],// الفواتير المدفوعه و اجمالى الفواتير
+    //              'data' => [69, 59]
+    //          ],
+    //          [
+    //              "label" => 'الفواتير الغير مدفوعة',
+    //              'backgroundColor' => ['rgba(255, 99, 132, 0.4)','rgba(54, 162, 235, 0.4)'],// الفواتير المدفوعه جزئيا و الفواتير غير المدفوعه
+    //              'data' => [65, 12]
+    //          ]
+    //      ])
+    //      ->options([]);
+
+    //     return view('home', compact('chartjs','chartjs2'));
+    // }
+    //=================احصائية نسبة تنفيذ الحالات======================
+
+
+
+    $count_all =invoices::count();
+    $count_invoices1 = invoices::where('Value_Status', 1)->count();
+    $count_invoices2 = invoices::where('Value_Status', 2)->count();
+    $count_invoices3 = invoices::where('Value_Status', 3)->count();
+
+    if($count_invoices2 == 0){
+        $nspainvoices2=0;
     }
+    else{
+        $nspainvoices2 = $count_invoices2/ $count_all*100;
+    }
+
+      if($count_invoices1 == 0){
+          $nspainvoices1=0;
+      }
+      else{
+          $nspainvoices1 = $count_invoices1/ $count_all*100;
+      }
+
+      if($count_invoices3 == 0){
+          $nspainvoices3=0;
+      }
+      else{
+          $nspainvoices3 = $count_invoices3/ $count_all*100;
+      }
+
+
+      $chartjs = app()->chartjs
+          ->name('barChartTest')
+          ->type('bar')
+          ->size(['width' => 350, 'height' => 200])
+          ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة','الفواتير المدفوعة جزئيا'])
+          ->datasets([
+              [
+                  "label" => "الفواتير الغير المدفوعة",
+                  'backgroundColor' => ['rgba(255, 99, 132, 0.4)'],
+                  'data' => [$nspainvoices2]
+              ],
+              [
+                  "label" => "الفواتير المدفوعة",
+                  'backgroundColor' => ["rgba(38, 185, 140, 0.5)"],
+                  'data' => [$nspainvoices1]
+              ],
+              [
+                  "label" => "الفواتير المدفوعة جزئيا",
+                  'backgroundColor' => ['rgba(54, 162, 235, 0.4)'],
+                  'data' => [$nspainvoices3]
+              ],
+
+
+          ])
+          ->options([]);
+
+
+      $chartjs_2 = app()->chartjs
+          ->name('pieChartTest')
+          ->type('pie')
+          ->size(['width' => 340, 'height' => 200])
+          ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة','الفواتير المدفوعة جزئيا'])
+          ->datasets([
+              [
+                  'backgroundColor' => ['rgba(54, 162, 235,0.6)', "rgba(54, 162, 235, 0.4)",'rgba(54, 162, 235, 0.2)'],
+                  'data' => [$nspainvoices2, $nspainvoices1,$nspainvoices3]
+              ]
+          ])
+          ->options([]);
+
+      return view('home', compact('chartjs','chartjs_2','chartjs2'));
+
+  }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,10 @@
 					<div class="left-content">
 						<div>
 						  <h2 class="main-content-title tx-24 mg-b-1 mg-b-lg-1">Hi, welcome back!</h2>
-						  <p class="mg-b-0">Sales monitoring dashboard template.</p>
+						  <p class="mg-b-0">Sales monitoring dashboard.</p>
 						</div>
 					</div>
-					<div class="main-dashboard-header-right">
+					<!-- <div class="main-dashboard-header-right">
 						<div>
 							<label class="tx-13">Customer Ratings</label>
 							<div class="main-star">
@@ -32,28 +32,28 @@
 							<label class="tx-13">Offline Sales</label>
 							<h5>783,675</h5>
 						</div>
-					</div>
+					</div> -->
 				</div>
 				<!-- /breadcrumb -->
 @endsection
 @section('content')
-				<!-- row -->
+				<!-- row الفواتير -->
 				<div class="row row-sm">
 					<div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
 						<div class="card overflow-hidden sales-card bg-primary-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TODAY ORDERS</h6>
+									<h6 class="mb-3 tx-12 text-white">اجمالى الفواتير</h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$5,74.12</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">{{number_format( \App\invoices::sum('Total') ,2)}} $</h4>
+											<p class="mb-0 tx-12 text-white op-7">{{ number_format( \App\invoices::count() , 0)}}</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> +427</span>
+											<span class="text-white op-7"> +100%</span>
 										</span>
 									</div>
 								</div>
@@ -65,17 +65,17 @@
 						<div class="card overflow-hidden sales-card bg-danger-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
+									<h6 class="mb-3 tx-12 text-white">الفواتير غير الدمفوعه</h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">{{number_format( \App\invoices::where('Value_Status','2')->sum('Total') ,2)}} $</h4>
+											<p class="mb-0 tx-12 text-white op-7">{{ number_format( \App\invoices::where('Value_Status','2')->count() , 0)}}</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -23.09%</span>
+											<span class="text-white op-7"> {{ round( (\App\invoices::where('Value_Status','2')->count() / \App\invoices::count())*100 ) , 2 }} %</span>
 										</span>
 									</div>
 								</div>
@@ -87,17 +87,17 @@
 						<div class="card overflow-hidden sales-card bg-success-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TOTAL EARNINGS</h6>
+									<h6 class="mb-3 tx-12 text-white">الفواتير الدمفوعه</h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$7,125.70</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">{{number_format( \App\invoices::where('Value_Status','1')->sum('Total') ,2)}} $</h4>
+											<p class="mb-0 tx-12 text-white op-7">{{ number_format( \App\invoices::where('Value_Status','1')->count() , 0)}}</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> 52.09%</span>
+											<span class="text-white op-7"> {{ round( (\App\invoices::where('Value_Status','1')->count() / \App\invoices::count())*100 ) , 2 }} %</span>
 										</span>
 									</div>
 								</div>
@@ -109,17 +109,17 @@
 						<div class="card overflow-hidden sales-card bg-warning-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">PRODUCT SOLD</h6>
+									<h6 class="mb-3 tx-12 text-white">الفواتير الدمفوعه جزئيا</h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">{{number_format( \App\invoices::where('Value_Status','3')->sum('Total') ,2)}} $</h4>
+											<p class="mb-0 tx-12 text-white op-7">{{ number_format( \App\invoices::where('Value_Status','3')->count() , 0)}}</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
+											<span class="text-white op-7"> {{ round( (\App\invoices::where('Value_Status','3')->count() / \App\invoices::count())*100 ) , 2 }} %</span>
 										</span>
 									</div>
 								</div>
@@ -141,39 +141,32 @@
 								</div>
 								<p class="tx-12 text-muted mb-0">Order Status and Tracking. Track your order from ship date to arrival. To begin, enter your order number.</p>
 							</div>
-							<div class="card-body">
-								<div class="total-revenue">
-									<div>
-									  <h4>120,750</h4>
-									  <label><span class="bg-primary"></span>success</label>
-									</div>
-									<div>
-									  <h4>56,108</h4>
-									  <label><span class="bg-danger"></span>Pending</label>
-									</div>
-									<div>
-									  <h4>32,895</h4>
-									  <label><span class="bg-warning"></span>Failed</label>
-									</div>
-								  </div>
-								<div id="bar" class="sales-bar mt-4"></div>
+							<div class="card-body"  style="width:75%;">
+								{!! $chartjs->render() !!}
 							</div>
 						</div>
 					</div>
 					<div class="col-lg-12 col-xl-5">
-						<div class="card card-dashboard-map-one">
-							<label class="main-content-label">Sales Revenue by Customers in USA</label>
-							<span class="d-block mg-b-20 text-muted tx-12">Sales Performance of all states in the United States</span>
-							<div class="">
-								<div class="vmap-wrapper ht-180" id="vmap2"></div>
+							<div class="card card-dashboard-map-one" >
+							<div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mb-0">Order status</h4>
+									<i class="mdi mdi-dots-horizontal text-gray"></i>
+								</div>
+								<p class="tx-12 text-muted mb-0"></p>
 							</div>
-						</div>
+								<div class="" style="width:100%;">
+									{!! $chartjs_2->render() !!}
+								</div>
+							</div>
 					</div>
+					
+					
 				</div>
 				<!-- row closed -->
 
 				<!-- row opened -->
-				<div class="row row-sm">
+				<!-- <div class="row row-sm">
 					<div class="col-xl-4 col-md-12 col-lg-12">
 						<div class="card">
 							<div class="card-header pb-1">
@@ -347,11 +340,11 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 				<!-- row close -->
 
 				<!-- row opened -->
-				<div class="row row-sm row-deck">
+				<!-- <div class="row row-sm row-deck">
 					<div class="col-md-12 col-lg-4 col-xl-4">
 						<div class="card card-dashboard-eight pb-2">
 							<h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>
@@ -436,7 +429,7 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 				<!-- /row -->
 			</div>
 		</div>
